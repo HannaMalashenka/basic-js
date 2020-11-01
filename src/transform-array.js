fa9fb8ec529249eada3b335644fb1241aa007b38
anika <hannamalashenka@gmail.com>
2020-11-01 23:29:40 +0300

    feat: solve chain and array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw  Error();
+  
+  let prevValue;
+  const newArr = arr.map((item, i) => {
+    if (arr[i+1] === '--discard-prev') {
+      prevValue = item;
+      return undefined;
+    } else if (arr[i-1] === '--discard-next') {
+      prevValue = undefined;
+      return undefined;
+    } else if (arr[i] === '--double-prev' && prevValue !== undefined) {
+      prevValue = item;
+      return arr[i-1];
+    } else if (arr[i] === '--double-next') {
+      prevValue = item;
+      return arr[i+1];
+    } else {
+      prevValue = item;
+      return item;
+    }        
+  })
+  return newArr.filter((i) => i !== undefined 
+  && i !== '--discard-prev' 
+  && i !== '--discard-next' 
+  && i !== '--double-prev');
 };
