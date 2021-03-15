45204f590590b79de94a04ced09986a4e890a046
anika <hannamalashenka@gmail.com>
2021-03-15 16:05:03 +0300

    fix: add additional checks for carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof(sampleActivity) !== 'string' || typeof(sampleActivity) !== NaN) {
+  if (typeof(sampleActivity) !== 'string' || 
+  	isNaN(+sampleActivity) ||
+    +sampleActivity <= 0 ||
+    +sampleActivity > MODERN_ACTIVITY
+  ) {
     return false;
   }
     return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
